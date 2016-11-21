767ee6a6a4c0bf036056906e92e4a2dfe8dd883b
Rustam Gasanov <rustamagasanov@gmail.com>
2016-11-21 01:57:16 +0100

    Added cli and client

--- lib/mrm/cli.rb
+++ lib/mrm/cli.rb
@@ -1,27 +1,31 @@
 module MRM
-  class Client
+  class CLI
     def initialize
       @config = YAML.load_file("#{Dir.home}/.mrmconfig")
     end
 
     def version_check
-      api.version_check
+      puts api.version_check.brown
     end
 
     def get_manifest(name, reference)
-      api.get_manifest(name, reference)
+      pp api.get_manifest(name, reference)
     end
 
     def list_repositories
-      api.list_repositories['repositories']
+      api.list_repositories['repositories'].each do |repo|
+        puts repo.brown
+      end
     end
 
     def list_tags(name)
-      api.list_tags(name)['tags']
+      api.list_tags(name)['tags'].each do |tag|
+        puts tag.brown
+      end
     end
 
     def delete_image(name, reference)
-      api.delete_image(name, reference)
+      puts api.delete_image(name, reference)
     end
 
     private
--- lib/mrm/version.rb
+++ lib/mrm/version.rb
@@ -2,7 +2,7 @@ module MRM
   class Version
     MAJOR = 1
     MINOR = 3
-    PATCH = 2
+    PATCH = 3
     PRE = nil
 
     class << self
